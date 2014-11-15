e11b941ac0939f0f58f7b58b9fc4e1595031e25f
Martin Drapeau <martindrapeau@gmail.com>
2014-11-15 07:24:15 -0500

    Camera now panning vertically too.

--- src/camera.js
+++ src/camera.js
@@ -11,13 +11,13 @@
 
   // Camera class
   // Ensures the hero is always in the viewport.
-  // Properly pans the world. For now, only horizontally.
+  // Properly pans the world.
   Backbone.Camera = Backbone.Model.extend({
     defaults: {
       left: 200,
       right: 400,
-      top: null,
-      bottom: null
+      top: 100,
+      bottom: 100
     },
     initialize: function(attributes, options) {
       this.setOptions(options || {});
@@ -28,31 +28,50 @@
 
       this.stopListening();
       if (this.subject && this.world) {
-        this.listenTo(this.subject, "change:x", this.maybePanX);
-        this.maybePanX();
+        this.listenTo(this.subject, "change:x change:y", this.maybePan);
+        this.maybePan();
       }
     },
-    maybePanX: function() {
+    maybePan: function() {
       var w = this.world.toShallowJSON(),
+          worldX = w.x,
+          worldY = w.y,
           worldWidth = w.width * w.tileWidth,
+          worldHeight = w.height * w.tileHeight,
           subjectX = this.subject.get("x") + w.x,
+          subjectY = this.subject.get("y") + w.y,
           subjectWidth = this.subject.get("tileWidth"),
+          subjectHeight = this.subject.get("tileHeight"),
           canvas = this.world.backgroundCanvas,
-          leftX = this.get("left"),
-          rightX = canvas.width - this.get("right");
+          left = this.get("left"),
+          right = canvas.width - this.get("right"),
+          top = this.get("top"),
+          bottom = canvas.height - this.get("bottom");
 
-      if (subjectX < leftX && w.x < 0) {
+      if (subjectX < left && w.x < 0) {
         // Pan right (to see more left)
-        var worldX = w.x + (leftX - subjectX);
+        worldX = w.x + (left - subjectX);
         if  (worldX > 0) worldX = 0;
-        this.world.set({x: worldX});
-      } else if (subjectX > rightX && w.x + worldWidth > canvas.width) {
+      } else if (subjectX > right && w.x + worldWidth > canvas.width) {
         // Pan left (to see more right)
-        var worldX = w.x - (subjectX - rightX);
+        worldX = w.x - (subjectX - right);
         if (worldX + worldWidth < canvas.width)
             worldX = -worldWidth + canvas.width;
-        this.world.set({x: worldX});
       }
+
+      if (subjectY < top && w.y < 0) {
+        // Pan down (to see more up)
+        worldY = w.y + (top - subjectY);
+        if  (worldY > 0) worldY = 0;
+      } else if (subjectY > bottom && w.y + worldHeight > canvas.height) {
+        // Pan up (to see more down)
+        worldY = w.y - (subjectY - bottom);
+        if (worldY + worldHeight < canvas.height)
+            worldY = -worldHeight + canvas.height;
+      }
+
+      if (worldX != w.x ||  worldY != w.y)
+        this.world.set({x: worldX, y: worldY});
     },
     update: function(dt) {
       return false;
